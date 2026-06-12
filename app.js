f0888cf1d12f45ee80237780c35059293a041446
alssanchezvega12-jpg <alssanchezvega12@gmail.com>
2026-06-12 12:14:02 -0600

    Actualización de index.html y style.css

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://inventario-backend.onrender.com/productos";
+const API_URL = "https://inventario-backend-4sl0.onrender.com/productos";
 
 // Función para consultar los datos guardados en MongoDB Atlas
 async function obtenerProductos() {
